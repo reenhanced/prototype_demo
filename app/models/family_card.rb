077125c6f42a3fda8ee17a522e027ba11d03c640
Valentino <valentino@reenhanced.com>
2012-09-21 14:11:22 -0400

    Required user to enter a unique email address and phone number for family cards.

--- app/models/family_card.rb
+++ app/models/family_card.rb
@@ -1,8 +1,10 @@
 class FamilyCard < ActiveRecord::Base
   belongs_to :user
   has_one :default_parent, :class_name => 'Parent', :dependent => :nullify
+
   attr_accessible :parent_first_name, :parent_last_name, :student_name, :phone, :email, :address1, :address2, :city, :state, :zip_code
 
+  validates_uniqueness_of :email, :phone
   def self.find_all_from_search(params={})
     conditions = {}
     params ||= {}
