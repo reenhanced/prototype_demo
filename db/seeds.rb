ac41c59a7a846e800d2dfe47e3dd924bf9ab4828
Nicholas Hance <nhance@reenhanced.com>
2012-09-25 09:08:11 -0400

    Database seeds for easier dev setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create!(email: 'test@example.com', password: 'password', password_confirmation: 'password')
+
+user.family_cards.create!(parent_first_name: 'John', parent_last_name: 'Smith', student_name: 'Sally Student', phone: '215-804-9408', email: 'smiths@example.com', address1: '312 West Broad Street', address2: 'In the back', city: 'Quakertown', state: 'PA', zip_code: '18951')
