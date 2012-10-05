ba8d0873047add56bebde6c72528d0cb644e83d1
Valentino <valentino@reenhanced.com>
2012-10-05 09:11:23 -0400

    Removed qualifier ids from call log mass assignment

--- app/models/call_log.rb
+++ app/models/call_log.rb
@@ -4,7 +4,7 @@ class CallLog < ActiveRecord::Base
 
   delegate :qualifier_ids, :qualifier_ids=, :to => :family_card
 
-  attr_accessible :message, :contact_id, :contact_type, :qualifier_ids, :recorded_at
+  attr_accessible :message, :contact_id, :contact_type, :recorded_at
 
   validates :message, :presence => true
 end
