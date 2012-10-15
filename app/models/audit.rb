9a5e050a07c89c2f4c06c70f56a32b3be634858b
Valentino <valentino@reenhanced.com>
2012-10-15 13:52:33 -0400

    Audit.with_associated_for

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -1,2 +1,14 @@
 class Audit < Audited::Adapters::ActiveRecord::Audit
+  def self.with_associated_for(auditor = nil)
+    return [] unless auditor
+    where(
+      arel_table[:auditable_id].eq(auditor.id).and(
+        arel_table[:auditable_type].eq(auditor.class.to_s)
+      ).or(
+        arel_table[:associated_id].eq(auditor.id).and(
+          arel_table[:associated_type].eq(auditor.class.to_s)
+        )
+      )
+    )
+  end
 end
