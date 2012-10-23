1c072cd617374d385a609d5da439c5de92eb1dc8
Valentino <valentino@reenhanced.com>
2012-10-23 11:08:06 -0400

    Fixed styles for navbar and audit labels

--- spec/helpers/application_helper_sepc.rb
+++ spec/helpers/application_helper_sepc.rb
@@ -2,19 +2,36 @@ require 'spec_helper'
 
 describe ApplicationHelper do
   context "instance methods" do
-    describe "#twitterized_flash_class" do
-      let(:twitter_flashes) do
+    describe "#bootstrap_class" do
+      let(:twitter_classes) do
         {
-          :alert => 'alert-block',
-          :error => 'alert-error',
-          :notice => 'alert-info',
-          :success => 'alert-success'
+          :alert => {
+            :alert   => 'alert-block',
+            :error   => 'alert-error',
+            :notice  => 'alert-info',
+            :success => 'alert-success'
+          },
+          :text => {
+            :negative => 'text-error',
+            :neutral  => 'text-warning',
+            :positive => 'text-success'
+          },
+          :label => {
+            :negative => 'label-important',
+            :neutral  => 'label-warning',
+            :positive => 'label-success',
+            :create   => 'label-info',
+            :update   => 'label-success',
+            :destroy  => 'label-important'
+          }
         }
       end
 
       it "returns the twitter-bootstrap version for each flash type" do
-        twitter_flashes.each do |flash_type, twitter_class|
-          helper.twitterized_flash_class(flash_type).should == twitter_class
+        twitter_classes.each do |label_type, twitter_classes|
+          twitter_classes.each do |type, twitter_class|
+            helper.bootstrap_class(type, type: label_type).should == twitter_class
+          end
         end
       end
     end
