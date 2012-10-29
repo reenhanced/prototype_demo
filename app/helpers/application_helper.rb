ff8823930000d9340284eabcd8e142c84ea57fe4
Valentino <valentino@reenhanced.com>
2012-10-29 16:55:10 -0400

    WIP - better address formats

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,32 @@ module ApplicationHelper
         type.to_s
     end
   end
+
+  def address_tag_for(object)
+    return "" unless object.respond_to?(:address1) and object.respond_to?(:address2) and
+                     object.respond_to?(:city)     and object.respond_to?(:state) and
+                     object.respond_to?(:zip_code)
+
+    street_address = content_tag(:span, object.address1, class: 'street-address')
+    street_address +=  tag(:br) if object.address1.present?
+
+    secondary_address = ""
+    if object.address2.present?
+      secondary_address = content_tag(:span, object.address2, class: 'extended-address') + tag(:br)
+    end
+
+    city = ""
+    if object.city.present?
+      city = content_tag(:span, object.city, class: 'locality')
+      city += ", " if object.state.present?
+    end
+
+    state       = content_tag(:span, object.state, class: 'region')
+    postal_code = content_tag(:span, object.zip_code, class: 'postal-code')
+
+    content_tag :address, class: 'adr' do
+      street_address + secondary_address + city + state + " " + postal_code
+    end
+
+  end
 end
